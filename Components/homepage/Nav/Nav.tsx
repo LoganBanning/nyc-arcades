0f426580155a1b41b2188f3bb583384748782222
Logan Banning <loganboganb@gmail.com>
2022-09-28 18:53:38 -0600

    worked on nav bar

--- Components/homepage/Nav/Nav.tsx
+++ Components/homepage/Nav/Nav.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import './Nav.css';
 
 const TopNav= () => {
 
   return (
     <div className='nav-container'>
       <div className='nav-items'>
-        <div className='menu-icon'></div>
+        <div className='menu-icon'><HiMenu /></div>
         <h1 className='nyc-arcades-text'>NYC ARCADES</h1>
-        <div className='search-icon'></div>
+        <div className='search-icon'> <BiSearch /></div>
       </div>
     </div>
   )
